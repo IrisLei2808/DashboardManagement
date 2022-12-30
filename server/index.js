6de93892bf3d2f2f183bff0621beb4524d659345
IrisLei2808 <duclt2808@gmail.com>
2022-12-30 00:03:30 -0800

    cm

---
+++ server/index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
+
+// data import 
+import User from './models/User.js';
+import {dataUser, dataProduct, dataProductStat, dataTransaction,} from './data/index.js';
+import Product from './models/Product.js';
+import ProductStat from './models/ProductStat.js';
+import Transaction from './models/Transaction.js';
+// configuration
+dotenv.config()
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"})) // sharing request
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// routes
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes)
+app.use("sales", salesRoutes);
+
+// mongo setup
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server port: ${PORT}`));
+   // Product.insertMany(dataProduct)
+  //  ProductStat.insertMany(dataProductStat)
+   // User.insertMany(dataUser)
+  // Transaction.insertMany(dataTransaction)
+}).catch((error) => console.log(`${error} did not connect`))
